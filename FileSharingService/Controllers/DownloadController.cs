a028de4bed8bae4ce5f3fbc6ff1aa25873966305
Nick Chobotar <nick.chobotar@outlook.com>
2018-06-10 20:06:21 -0400

    working project

---
+++ FileSharingService/Controllers/DownloadController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace FileSharingService.Controllers
+{
+    public class DownloadController : Controller
+    {
+        public FileResult Download(string fileID)
+        {
+            var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileID);
+            byte[] fileBytes = System.IO.File.ReadAllBytes(path);
+            string fileName = fileID;
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+
+        }
+    }
+}
--- FileSharingService/Controllers/UploadController.cs
+++ FileSharingService/Controllers/UploadController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using FileSharingService.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,13 +12,38 @@ namespace FileSharingService.Controllers
     {
         // GET: Upload
 
-       [HttpPost]
-        public ActionResult Upload()
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
         {
-            return View();
+            string fileName = Helpers.Helper.RandomString(5);
+
+            if (file.ContentLength > 0)
+            {               
+                var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                file.SaveAs(path);
+            }
+            return RedirectToAction("UploadComplete", new {fileID = fileName});
         }
 
-   
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileID"></param>
+        /// <returns></returns>
+        public ActionResult UploadComplete(string fileID)
+        {
+            UploadCompleteViewModel ViewModel = new UploadCompleteViewModel();
+            ViewModel.fileID = fileID;
+
+            //string link = HtmlHelper.GenerateLink(
+            //    this.ControllerContext.RequestContext, 
+            //    System.Web.Routing.RouteTable.Routes, 
+            //    "Download", null, "Download", "Download", null, null);
+
+
+            return View(ViewModel);
+        }
+            
 
     }
 }
